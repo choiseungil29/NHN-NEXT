9e32aa0e7b4046d9feaf3d592fab919a7f6e29f4
System Administrator <root@Poohui-MacBook-Pro.local>
2014-07-16 21:25:35 +0900

    프로그래밍 연습 프로젝트 최초 커밋.

---
+++ ProgrammingPractice/ProgrammingPractice/ProgrammingPractice/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  ProgrammingPractice
+//
+//  Created by Pooh on 2014. 7. 16..
+//  Copyright (c) 2014년 Pooh. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
